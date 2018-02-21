04c79e52cec36671eef119478bef8dc27f31822b
Aniket <aniketkumartripathi@gmail.com>
2018-02-21 23:11:14 +0530

    Fix# Number of bricks and initial tank spawn point

--- src/main/java/environment/GameMap.java
+++ src/main/java/environment/GameMap.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import objects.Brick;
 import objects.GameObject;
 import objects.Tank;
+import windows.Frames;
 
 public class GameMap {
 
@@ -42,7 +43,7 @@ public class GameMap {
 		switch(game_map_template){
 		case  GAME_MAP_TEMPLATE1:{
 		// set number of bricks	
-			numberOfBricks = 16;
+			numberOfBricks = Frames.WIDTH/Brick.BRICK_IMAGE_WIDTH + 1;
 			
 		// set bricks regions	
 			for(int i = 0; i < numberOfBricks; i++)
@@ -50,7 +51,7 @@ public class GameMap {
 		
 		//set tank regions first tank begins from 3rd region and second tank begins from 12th region
 			tank1Region = new Region(Brick.BRICK_IMAGE_WIDTH/2 + 3* Brick.BRICK_IMAGE_WIDTH, gameRegion.height - Brick.BRICK_IMAGE_HEIGHT - Tank.TANK_IMAGE_HEIGHT/2, Tank.TANK_IMAGE_WIDTH, Tank.TANK_IMAGE_HEIGHT);
-			tank2Region = new Region(Brick.BRICK_IMAGE_WIDTH/2 + 12* Brick.BRICK_IMAGE_WIDTH, gameRegion.height - Brick.BRICK_IMAGE_HEIGHT - Tank.TANK_IMAGE_HEIGHT/2, Tank.TANK_IMAGE_WIDTH, Tank.TANK_IMAGE_HEIGHT);
+			tank2Region = new Region(Brick.BRICK_IMAGE_WIDTH/2 + (numberOfBricks - 1 - 3)* Brick.BRICK_IMAGE_WIDTH, gameRegion.height - Brick.BRICK_IMAGE_HEIGHT - Tank.TANK_IMAGE_HEIGHT/2, Tank.TANK_IMAGE_WIDTH, Tank.TANK_IMAGE_HEIGHT);
 		// set gravity , left and right wind
 			gravity = 1; leftWind = 0; rightWind = 0;
 		
